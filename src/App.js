44e384455f488e41ed3c113f0195b2a08a43edc9
jorgebp226 <jorgebp226@gmail.com>
2024-10-03 16:58:13 +0200

    Cambios en la Auth con la nueva documentación 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 // src/App.js
 import React from 'react';
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports'; // Asegúrate de que este archivo existe y está correctamente configurado
+import { Amplify, generateClient } from 'aws-amplify'; // Importamos Amplify y generateClient
+import awsExports from './aws-exports'; // Archivo de configuración de Amplify
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Calendar from './components/Calendar';
 import Progress from './components/Progress';
 import Navbar from './components/Navbar';
-import '@aws-amplify/ui-react/styles.css'; // Estilos por defecto para los componentes de Amplify
+import '@aws-amplify/ui-react/styles.css'; // Estilos por defecto de Amplify UI
 
 // Configura Amplify con la configuración generada por 'amplify push'
 Amplify.configure(awsExports);
+
+// Genera el cliente GraphQL para interactuar con la API
+const client = generateClient();
 
 function App() {
   return (
