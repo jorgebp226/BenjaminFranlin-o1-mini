0cfb0d8234a9692de4710ec1d7ba9673a8b840ac
jorgebp226 <jorgebp226@gmail.com>
2024-10-03 16:26:11 +0200

    Cambios en las queries definidas

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,25 +1,6 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const getSemana = /* GraphQL */ `
-  query GetSemana($userId: String!, $semana: String!) {
-    getSemana(userId: $userId, semana: $semana) {
-      id
-      userId
-      semana
-      virtudObjetivo
-      dias {
-        idVirtud
-        estado
-        __typename
-      }
-      createdAt
-      updatedAt
-      owner
-      __typename
-    }
-  }
-`;
 export const getVirtud = /* GraphQL */ `
   query GetVirtud($id: ID!) {
     getVirtud(id: $id) {
@@ -42,6 +23,47 @@ export const listVirtuds = /* GraphQL */ `
       items {
         id
         nombre
+        createdAt
+        updatedAt
+        owner
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getSemana = /* GraphQL */ `
+  query GetSemana($id: ID!) {
+    getSemana(id: $id) {
+      id
+      userId
+      semana
+      virtudObjetivo
+      dias {
+        idVirtud
+        estado
+        __typename
+      }
+      createdAt
+      updatedAt
+      owner
+      __typename
+    }
+  }
+`;
+export const listSemanas = /* GraphQL */ `
+  query ListSemanas(
+    $filter: ModelSemanaFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listSemanas(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        userId
+        semana
+        virtudObjetivo
         createdAt
         updatedAt
         owner
